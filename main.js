972d9b02718cc08d23cdbe89897ddb049a1ec57d
Andanchenko Maria <andanchenko.maria@gmail.com>
2019-06-01 17:18:53 +0300

    Add Default Weathet

--- main.js
+++ main.js
@@ -2,18 +2,42 @@ window.addEventListener("load", function(){
 var usd=document.querySelector('.usd');
 var eur=document.querySelector('.eur');
 var rub=document.querySelector('.rub');
-var myArr;
-var myObj;
+var defaultCity=document.querySelector('.defaultCity');
+var rateArr=[];
 var xhttp=new XMLHttpRequest();
+var defaultWeathet={};
+var lat, lon;
 
 class ViewExchangeRate{
-    show(myArr){
-        usd.innerHTML=myArr[0].buy;
-        eur.innerHTML=myArr[1].buy;
-        rub.innerHTML=myArr[2].buy;
+    showRate(rateArr){
+        usd.innerHTML=rateArr[0].buy;
+        eur.innerHTML=rateArr[1].buy;
+        rub.innerHTML=rateArr[2].buy;
     }
  }
- 
+ class ViewDefaultWeathet{
+  showDefaultWeathet(defaultWeathet){
+      console.log(defaultWeathet);
+      let city=document.createElement('h3');
+      let weatherDesc=document.createElement('p');
+      let pressure=document.createElement('p');
+      let humidity=document.createElement('p');
+      let clouds=document.createElement('p');
+      city.innerText=`Weathet in ${defaultWeathet.name} is:`;
+      weatherDesc.innerText=
+      `${defaultWeathet.weather[0].description} air temperature is ${defaultWeathet.main.temp} K`;
+      pressure.innerText=`pressure is ${defaultWeathet.main.pressure} Pa`;
+      humidity.innerText=`humidity is ${defaultWeathet.main.humidity} %`;
+      clouds.innerText=`clouds is ${defaultWeathet.clouds.all}%`
+      defaultCity.appendChild(city);
+      defaultCity.appendChild(weatherDesc);
+      defaultCity.appendChild(pressure);
+      defaultCity.appendChild(humidity);
+      defaultCity.appendChild(clouds);
+  }
+}
+
+
 class ModelExchangeRate{
     constructor(viewExchangeRate){
         this.viewExchangeRate=viewExchangeRate;
@@ -23,8 +47,8 @@ class ModelExchangeRate{
         xhttp.send();
         xhttp.addEventListener('readystatechange', function(){
           if(xhttp.readyState==4&&xhttp.status==200){
-            myArr=JSON.parse(xhttp.response);
-            viewExchangeRate.show(myArr);
+            rateArr=JSON.parse(xhttp.response);
+            viewExchangeRate.showRate(rateArr);
           } else{
             console.log(xhttp.readyState, xhttp.status);
           }
@@ -33,18 +57,59 @@ class ModelExchangeRate{
     }
  
  }
+
+ class ModelDefaultWeathet{
+  constructor(view){
+      this.view=view;
+  }
+  getCity(getVal){
+      function success(position, getVal) {
+          lat = position.coords.latitude.toFixed(2);
+          lon = position.coords.longitude.toFixed(2);
+          modelDefaultWeathet.getVal();
+      };
+       
+      function error(obj) {
+          console.log("Ошибка при определении положения");
+      };
+      navigator.geolocation.getCurrentPosition(success, error);
+  }
+  getVal(){
+      var XHR = ("onload" in new XMLHttpRequest()) ? XMLHttpRequest : XDomainRequest; 
+      var xhr = new XHR(); 
+      xhr.open('GET', `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=694e5f0d692f8622e5ca576e4bcce755`, true); 
+      xhr.onload = function() { 
+          defaultWeathet=JSON.parse(xhr.response); 
+          viewDefaultWeathet.showDefaultWeathet(defaultWeathet);
+      } 
+      xhr.onerror = function() { 
+          console.log(xhr.readyState, xhr.status) 
+      } 
+      xhr.send();
+  }
+}
+
+
+
  class Controller{
-    constructor(modelExchangeRate){
+    constructor(modelExchangeRate, modelDefaultWeathet){
         this.modelExchangeRate=modelExchangeRate;
+        this.modelDefaultWeathet=modelDefaultWeathet;
     }
     makeExchangeRate(){
         modelExchangeRate.getVal();
         window.setInterval(modelExchangeRate.getVal,36000);   
     }
+    makeDefaultWeathet(){
+      modelDefaultWeathet.getCity(modelDefaultWeathet.getVal);
+    }
  
  }
   var viewExchangeRate = new ViewExchangeRate;
   var modelExchangeRate=new ModelExchangeRate(viewExchangeRate);
-  var controller = new Controller(modelExchangeRate);
+  var viewDefaultWeathet = new ViewDefaultWeathet;
+  var modelDefaultWeathet=new ModelDefaultWeathet(viewDefaultWeathet);
+  var controller = new Controller(modelExchangeRate,modelDefaultWeathet);
   controller.makeExchangeRate();
+  controller.makeDefaultWeathet();
 });
